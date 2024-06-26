d252aba6d522f3a60910a7e66a77afe619f4a504
arkzuse <88483186+arkzuse@users.noreply.github.com>
2024-06-26 18:13:22 +0530

    changed type

--- backend/board_test.go
+++ backend/board_test.go
@@ -7,37 +7,37 @@ import (
 func TestPlacePiece(t *testing.T) {
 	b := newBoard()
 
-	err := b.placePiece(0, "X")
+	err := b.placePiece(0, 0)
 	if err != nil {
 		t.Errorf("Expected no error, got %v", err)
 	}
 
-	err = b.placePiece(0, "O")
+	err = b.placePiece(0, 1)
 	if err != nil {
 		t.Errorf("Expected no error, got %v", err)
 	}
 
 	for i := 0; i < 4; i++ {
-		err = b.placePiece(1, "X")
+		err = b.placePiece(1, 0)
 		if err != nil {
 			t.Errorf("Expected no error, got %v", err)
 		}
 	}
 
 	w := b.checkBoard()
-	if w != "X" {
-		t.Errorf("Expected X to win, got %s", w)
+	if w != 0 {
+		t.Errorf("Expected X to win, got %d", w)
 	}
 
 	/// test diagonal win
 	b = newBoard()
 
 	for i := 0; i < 4; i++ {
-		b.Board[i][i] = "X"
+		b.Board[i][i] = 0
 	}
 
 	w = b.checkBoard()
-	if w != "X" {
-		t.Errorf("Expected X to win, got %s", w)
+	if w != 0 {
+		t.Errorf("Expected X to win, got %d", w)
 	}
 }
