2348af277b743c53109d2d361f65cea4b01c3563
arkzuse <88483186+arkzuse@users.noreply.github.com>
2024-07-01 00:38:25 +0530

    add: game manager

---
+++ backend/game_manager.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/google/uuid"
+
+type GameManager struct {
+	games map[uuid.UUID]*Game
+}
+
+func NewGameManager() *GameManager {
+	return &GameManager{
+		games: make(map[uuid.UUID]*Game),
+	}
+}
+
+func (gm *GameManager) NewGame() *Game {
+	g := NewGame()
+	gm.games[g.ID] = g
+	return g
+}
+
+func (gm *GameManager) GetGame(id uuid.UUID) *Game {
+	return gm.games[id]
+}
